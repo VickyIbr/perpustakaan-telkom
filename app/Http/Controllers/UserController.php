48852e4b1c0a5067354a495049d2d33b5c6e4b81
Vicky Ibrahimovic <149799887+VickyIbr@users.noreply.github.com>
2026-02-24 12:47:20 +0700

    Fix authorization check in destroy method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -82,7 +82,7 @@ class UserController extends Controller
 
     public function destroy(User $user)
     {
-        if ($user->id == User::user()->id) {
+        if ($user->id == Auth::user()->id) {
             abort(403,'');
         }
 
